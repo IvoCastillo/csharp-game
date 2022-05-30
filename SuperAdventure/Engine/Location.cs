df52d63f44fa570231f17d6fd07bf2df60b41bea
Ivo Saavedra Castillo <Ivo.Saavedra@ilomar.com>
2022-05-30 13:58:29 +0200

    adding constructors to the quest & location logic

--- SuperAdventure/Engine/Location.cs
+++ SuperAdventure/Engine/Location.cs
@@ -4,8 +4,14 @@ using System.Text;
 
 namespace Engine
 {
-    class Location
+    public class Location
     {
+        public Location(int id, string name, string description)
+        {
+            ID = id;
+            Name = name;
+            Description = description;
+        }
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
